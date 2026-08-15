25c01e7345c67b728178057a1091e591156a35e9
Yohannes mitike <jonmersha@gmail.com>
2026-08-16 02:42:40 +0300

    https

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 // export const API_BASE_URL = 'http://10.8.100.239';
 //export const API_BASE_URL = 'http://localhost:8000';
 // export const API_BASE_URL = 'http://localhost';
-export const API_BASE_URL = '192.168.0.105'
+export const API_BASE_URL = 'http://192.168.0.105'
